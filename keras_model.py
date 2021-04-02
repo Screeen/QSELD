c9f000af9763ab10ffba864199b4dca6d0befdc7
Giovanni Bologni <giovannibologni@gmail.com>
2021-04-02 19:27:09 +0200

    reformat code

--- keras_model.py
+++ keras_model.py
@@ -214,7 +214,6 @@ def temporal_block(inp, num_filters_gru=0, dropout=0, recurrent_type='gru', data
     print(f"Temporal block {recurrent_type} begins")
     if recurrent_type == 'gru':
         input_output = temporal_block_gru(inp, num_filters_gru, dropout, data_in, input_data_format)
-
     elif recurrent_type == 'tcn':
         input_output = temporal_block_guirguis(inp, nb_tcn_filt_dilated_, nb_tcn_blocks_, spatial_dropout_rate,
                                                use_quaternions_,
@@ -229,17 +228,11 @@ def temporal_block(inp, num_filters_gru=0, dropout=0, recurrent_type='gru', data
     return input_output
 
 
-def output_block(inp, out_shape, dropout=0, fnn_size=[0], dense_type='Dense'):
-    # if dense_type == 'QDense':
-    #     DenseGeneric = QuaternionDense
-    # else:
-    #     DenseGeneric = Dense
-
-    DenseGeneric = Dense
+def output_block(inp, out_shape, dropout=0, fnn_size=[0]):
 
     doa = inp
     for nb_fnn_filter in fnn_size:
-        doa = TimeDistributed(DenseGeneric(nb_fnn_filter))(doa)
+        doa = TimeDistributed(Dense(nb_fnn_filter))(doa)
         doa = Dropout(dropout)(doa)
 
     num_units_doa = out_shape[1][-1]
@@ -249,7 +242,7 @@ def output_block(inp, out_shape, dropout=0, fnn_size=[0], dense_type='Dense'):
     # SED
     sed = inp
     for nb_fnn_filter in fnn_size:
-        sed = TimeDistributed(DenseGeneric(nb_fnn_filter))(sed)
+        sed = TimeDistributed(Dense(nb_fnn_filter))(sed)
         sed = Dropout(dropout)(sed)
     num_units_sed = out_shape[0][-1]
     sed = TimeDistributed(Dense(num_units_sed))(sed)
@@ -258,33 +251,47 @@ def output_block(inp, out_shape, dropout=0, fnn_size=[0], dense_type='Dense'):
     return sed, doa
 
 
-def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, pool_size,
+def get_model(input_shape, output_shape, dropout_rate, pool_size,
               rnn_size, fnn_size, weights, params, data_format='channels_first'):
     # model definition
     keras.backend.set_image_data_format(data_format)
-    spec_start = Input(shape=(data_in[-3], data_in[-2], data_in[-1]))
+    spec_start = Input(shape=(input_shape[-3], input_shape[-2], input_shape[-1]))
 
     ##
     spatial_dropout = params['spatial_dropout_rate']
     recurrent_type = params['recurrent_type']
     nb_tcn_filt_ = params['nb_tcn_filt']  # num_conv_filters_tcn
     nb_tcn_blocks_ = params['nb_tcn_blocks']
+    use_quaternions_ = params['use_quaternions']
+
+    assert (data_format == "channels_last")
+    nb_cnn2d_filt = params['nb_cnn2d_filt'] if not use_quaternions_ else params['nb_cnn2d_filt'] // 4
 
     print(f"K.int_shape(spec_start) {K.int_shape(spec_start)}")
     spec_cnn = spec_start
     for i, this_pool_size in enumerate(pool_size):
-        spec_cnn = Conv2D(filters=nb_cnn2d_filt, kernel_size=(3, 3), padding='same')(spec_cnn)
+        if use_quaternions_:
+            if i == 0:
+                spec_cnn = QuaternionConv2D(input_shape=input_shape, filters=nb_cnn2d_filt, kernel_size=(3, 3),
+                                            padding='same',
+                                            data_format=data_format)(spec_cnn)
+            else:
+                spec_cnn = QuaternionConv2D(filters=nb_cnn2d_filt, kernel_size=(3, 3), padding='same',
+                                            data_format=data_format)(spec_cnn)
+        else:
+            spec_cnn = Conv2D(filters=nb_cnn2d_filt, kernel_size=(3, 3), padding='same')(spec_cnn)
+
         spec_cnn = BatchNormalization()(spec_cnn)
         spec_cnn = Activation('relu')(spec_cnn)
         spec_cnn = MaxPooling2D(pool_size=(1, this_pool_size))(spec_cnn)
         spec_cnn = Dropout(dropout_rate)(spec_cnn)
 
     spec_cnn = temporal_block(spec_cnn, num_filters_gru=rnn_size, dropout=dropout_rate,
-                              recurrent_type=recurrent_type, data_in=data_in, input_data_format=data_format,
+                              recurrent_type=recurrent_type, data_in=input_shape, input_data_format=data_format,
                               spatial_dropout_rate=spatial_dropout, nb_tcn_filt_dilated_=nb_tcn_filt_,
-                              nb_tcn_blocks_=nb_tcn_blocks_)
+                              nb_tcn_blocks_=nb_tcn_blocks_, use_quaternions_=use_quaternions_)
 
-    sed, doa = output_block(spec_cnn, out_shape=data_out, dropout=dropout_rate, fnn_size=fnn_size)
+    sed, doa = output_block(spec_cnn, out_shape=output_shape, dropout=dropout_rate, fnn_size=fnn_size)
 
     doa_objective = params['doa_objective']
     model = None
@@ -315,6 +322,7 @@ def masked_mse(y_gt, model_out):
     return keras.backend.sqrt(keras.backend.sum(keras.backend.square(
         y_gt - model_out[:, :, num_classes:]) * sed_out))/keras.backend.sum(sed_out)
 
+
 def load_seld_model(model_file, doa_objective):
     if doa_objective is 'mse':
         return load_model(model_file)
@@ -324,48 +332,3 @@ def load_seld_model(model_file, doa_objective):
         print('ERROR: Unknown doa objective: {}'.format(doa_objective))
         exit()
 
-
-def get_model_quaternion(inp_shape, out_shape, params):
-    inp = Input(shape=(inp_shape[-3], inp_shape[-2], inp_shape[-1]))
-    print(f"K.int_shape(spec_cnn) {K.int_shape(inp)}")
-
-    nb_cnn2d_filt = params['nb_cnn2d_filt'] // 4
-    nb_tcn_filt_ = params['nb_tcn_filt']
-
-    pool_size = params['pool_size']
-    dropout_rate = params['dropout_rate']
-
-    data_format = params['data_format']
-    assert (data_format == "channels_last")
-
-    fnn_size = params['fnn_size']
-    loss_weights = params['loss_weights']
-    spatial_do = params['spatial_dropout_rate']
-
-    spec_cnn = inp
-    for i, convCnt in enumerate(pool_size):
-        if i == 0:
-            spec_cnn = QuaternionConv2D(input_shape=inp_shape, filters=nb_cnn2d_filt, kernel_size=(3, 3),
-                                        padding='same',
-                                        data_format=data_format)(spec_cnn)
-        else:
-            spec_cnn = QuaternionConv2D(filters=nb_cnn2d_filt, kernel_size=(3, 3), padding='same',
-                                        data_format=data_format)(spec_cnn)
-
-        spec_cnn = BatchNormalization()(spec_cnn)
-        spec_cnn = Activation('relu')(spec_cnn)
-        spec_cnn = MaxPooling2D(pool_size=(pool_size[i], 1))(spec_cnn)
-        spec_cnn = Dropout(dropout_rate)(spec_cnn)
-
-    spec_cnn = temporal_block(spec_cnn, dropout=dropout_rate, input_data_format=data_format,
-                              recurrent_type=params['recurrent_type'], data_in=inp_shape, spatial_dropout_rate=spatial_do,
-                              nb_tcn_filt_dilated_=nb_tcn_filt_, nb_tcn_blocks_=params['nb_tcn_blocks'],
-                              use_quaternions_=True)
-
-    sed, doa = output_block(spec_cnn, out_shape=out_shape, dropout=dropout_rate, fnn_size=fnn_size,
-                            dense_type='QDense')
-
-    model = Model(inputs=inp, outputs=[sed, doa])
-    model.compile(optimizer=Adam(), loss=['binary_crossentropy', 'mse'], loss_weights=loss_weights)
-    model.summary()
-    return model
